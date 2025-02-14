5fb917cc690972c5785be6c06ca49e121634ba03
Arush <arushyadlapati.lwsd@gmail.com>
2025-02-13 22:42:02 -0800

    Fix base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "WCI Website",
   description: "FTC West Coast Invitational Website",
-  base: '/repo/',
+  base: '/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
