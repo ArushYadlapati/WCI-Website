3f4990085d596600e7eef7ee0941b259a13dc986
Arush <arushyadlapati.lwsd@gmail.com>
2025-02-14 11:54:37 -0800

    add custom navbar color

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  // title: "WCI-Website",
   description: "FTC West Coast Invitational Website",
   base: '/WCI-Website/',
   themeConfig: {
@@ -36,11 +35,24 @@ export default defineConfig({
       :root {
         --vp-nav-height: 105px;  /* whatever navbar height you want */
       }
+      .VPNav {
+        background-color: #1B2C50 !important;
+      }
       .VPNavBar {
         padding: 0 24px;
+        background-color: #1B2C50 !important;
       }
       .VPNavBar .logo {
-        height: 100px;  /* whatever wci logo height you want */
+        height: 100px; /* whatever wci logo height you want */
+      }
+      .VPNavBarMenuLink {
+        color: white !important;
+      }
+      .VPNavBarHamburger .box {
+        border-color: white !important;
+      }
+      .VPNavBar .content-body {
+        background-color: #1B2C50 !important;
       }
     `]
   ],
